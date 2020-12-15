bc314d6b2556d11f3cfca8cd88a0b6a667ff8db9
Wes Young <wesley.young@du.edu>
2020-12-15 12:33:44 -0700

    Added initial function to JS

--- employee.js
+++ employee.js
@@ -9,7 +9,28 @@ const connection = mysql.createConnection({
   database: "company_db",
 });
 
+const menuOptions = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      name: "userSelection",
+      choices: ["Departments", "Employees", "Exit"],
+    })
+    .then((data) => {
+      switch (data.userSelection) {
+        case "Departments":
+          console.log("1");
+        case "Employees":
+          console.log("2");
+        case "Exit":
+          connection.end();
+      }
+    });
+};
+
 connection.connect((err) => {
   if (err) throw err;
   console.log(`Connection at id ${connection.threadId}`);
+  menuOptions();
 });
