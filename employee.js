a18d0fbd2cc69263065368368d9304b4f77155c0
Wes Young <wesley.young@du.edu>
2020-12-15 16:55:45 -0700

    Added addDepartments function

--- employee.js
+++ employee.js
@@ -15,12 +15,15 @@ const menuOptions = () => {
       type: "list",
       message: "What would you like to do?",
       name: "userSelection",
-      choices: ["Departments", "Employees", "Exit"],
+      choices: ["Add department", "Employees", "Exit"],
     })
     .then((data) => {
       switch (data.userSelection) {
+        case "Add department":
+          return addDepartment();
         case "Departments":
           console.log("1");
+          break;
         case "Employees":
           console.log("2");
         case "Exit":
@@ -28,6 +31,23 @@ const menuOptions = () => {
       }
     });
 };
+
+function addDepartment() {
+  inquirer
+    .prompt({
+      message: "What is the name of the department you would like to add?",
+      name: "newDepartment",
+    })
+    .then((data) => {
+      connection.query(
+        "INSERT INTO departments SET ?",
+        {
+          name: data.newDepartment,
+        },
+        menuOptions()
+      );
+    });
+}
 
 connection.connect((err) => {
   if (err) throw err;
